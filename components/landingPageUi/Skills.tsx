43971afe2e4a546d1a4c3ed471b6c9a8a0889e2a
Promise Uzor <uzorpromiseokwudiri@gmail.com>
2024-12-15 22:45:15 +0000

    `feat: Added @radix-ui/react-tooltip dependency and updated package-lock.json and package.json files to reflect the change`

--- components/landingPageUi/Skills.tsx
+++ components/landingPageUi/Skills.tsx
@@ -4,6 +4,12 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { Skeleton } from "@/components/ui/skeleton";
 import { motion } from "framer-motion";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 interface Skill {
   id: string;
@@ -53,75 +59,84 @@ const Skills: React.FC<SkillsProps> = ({ skills }) => {
         Skills
       </h3>
       <div className="grid grid-cols-3 sm:grid-cols-6 lg:grid-cols-8 justify gap-10 w-full">
-        {loading
-          ? // Skeleton Loading
-            Array(8)
-              .fill(null)
-              .map((_, index) => (
-                <div
-                  key={index}
-                  className="flex flex-col items-center text-center space-y-4"
-                >
-                  {/* Skeleton Icon */}
-                  <Skeleton className="w-[50px] h-[50px] rounded-md" />
-                  {/* Skeleton Name */}
-                  <Skeleton className="w-[80px] h-[20px] rounded-full" />
-                  {/* Skeleton Progress Bar */}
-                  <Skeleton className="w-[60px] h-[10px] rounded-full" />
-                </div>
-              ))
-          : // Skills Data Rendering
-          Array.isArray(skills) && skills.length > 0 ? (
-            skills.map((skill) => {
-              // Calculate the percentage based on the score
-              const percentage = Math.min(skill.score, 100);
-  
-              // Determine the color based on the score
-              let color = "bg-red-500";
-              if (percentage >= 75) color = "bg-green-500";
-              else if (percentage >= 50) color = "bg-yellow-500";
-              else if (percentage >= 25) color = "bg-orange-500";
-  
-              return (
-                <motion.div
-                  key={skill.id}
-                  className="flex flex-col items-center text-center space-y-4"
-                  variants={skillVariants}
-                  initial="hidden"
-                  whileInView="visible"
-                  viewport={{ once: true, amount: 0.5 }}
-                >
-                  {/* Skill Icon */}
-                  {skill.image !== null && (
-                    <Image
-                      src={skill.image}
-                      alt={`${skill.name} Icon`}
-                      width={50}
-                      height={50}
-                      className="rounded-md"
-                    />
-                  )}
-                  {/* Skill Name */}
-                  <h3 className="text-lg font-semibold text-gray-200 dark:text-gray-400">
-                    {skill.name}
-                  </h3>
-                  {/* Progress Bar */}
-                  <div className="w-[60px] bg-gray-600 dark:bg-gray-700 shadow-inner border rounded-full h-2 overflow-hidden">
-                    <motion.div
-                      className={`${color} h-full rounded-full`}
-                      custom={percentage}
-                      variants={progressBarVariants}
-                      initial="hidden"
-                      whileInView="visible"
-                      viewport={{ once: true, amount: 0.5 }}
-                    ></motion.div>
-                  </div>
-                </motion.div>
-              );
-            })
-          ) : (
-            <p>No skills available.</p>
-          )}
+        {loading ? (
+          // Skeleton Loading
+          Array(8)
+            .fill(null)
+            .map((_, index) => (
+              <div
+                key={index}
+                className="flex flex-col items-center text-center space-y-4"
+              >
+                {/* Skeleton Icon */}
+                <Skeleton className="w-[50px] h-[50px] rounded-md" />
+                {/* Skeleton Name */}
+                <Skeleton className="w-[80px] h-[20px] rounded-full" />
+                {/* Skeleton Progress Bar */}
+                <Skeleton className="w-[60px] h-[10px] rounded-full" />
+              </div>
+            ))
+        ) : // Skills Data Rendering
+        Array.isArray(skills) && skills.length > 0 ? (
+          skills.map((skill) => {
+            // Calculate the percentage based on the score
+            const percentage = Math.min(skill.score, 100);
+
+            // Determine the color based on the score
+            let color = "bg-red-500";
+            if (percentage >= 75) color = "bg-green-500";
+            else if (percentage >= 50) color = "bg-yellow-500";
+            else if (percentage >= 25) color = "bg-orange-500";
+
+            return (
+              <motion.div
+                key={skill.id}
+                className="flex flex-col items-center text-center space-y-4"
+                variants={skillVariants}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+              >
+                {/* Skill Icon */}
+                {skill.image !== null && (
+                  <Image
+                    src={skill.image}
+                    alt={`${skill.name} Icon`}
+                    width={50}
+                    height={50}
+                    className="rounded-md"
+                  />
+                )}
+                {/* Skill Name */}
+                <h3 className="text-lg font-semibold text-gray-200 dark:text-gray-400">
+                  {skill.name}
+                </h3>
+                {/* Progress Bar */}
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <div className="w-[60px] bg-gray-600 dark:bg-gray-700 shadow-inner border rounded-full h-2 overflow-hidden">
+                        <motion.div
+                          className={`${color} h-full rounded-full`}
+                          custom={percentage}
+                          variants={progressBarVariants}
+                          initial="hidden"
+                          whileInView="visible"
+                          viewport={{ once: true, amount: 0.5 }}
+                        ></motion.div>
+                      </div>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <strong>{skill.score}%</strong>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </motion.div>
+            );
+          })
+        ) : (
+          <p>No skills available.</p>
+        )}
       </div>
     </div>
   );
