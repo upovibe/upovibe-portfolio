7e617402c498b9f778758a4432c696e1edefa297
upovibe <uzorpromiseokwudiri@gmail.com>
2025-01-02 16:44:31 +0000

    Refactor ContactMe component: remove loading state and animations, simplify contact link rendering

--- components/landingPageUi/ContactMe.tsx
+++ components/landingPageUi/ContactMe.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { Skeleton } from "@/components/ui/skeleton";
-import { motion } from "framer-motion";
 import ContactForm from "@/components/form/ContactForm";
 import { Separator } from "../ui/separator";
 
@@ -20,34 +18,6 @@ interface ContactMeProps {
 }
 
 const ContactMe: React.FC<ContactMeProps> = ({ contactlinks }) => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000); // Simulate loading time of 2 seconds
-  }, []);
-
-  // Animation Variants
-  const containerVariants = {
-    hidden: { opacity: 1 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.2, // Delay between each contact link animation
-      },
-    },
-  };
-
-  const itemVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: { duration: 0.6, ease: "easeOut" },
-    },
-  };
-
   return (
     <div
       id="contact"
@@ -57,47 +27,34 @@ const ContactMe: React.FC<ContactMeProps> = ({ contactlinks }) => {
         Contact Me
       </h3>
       <ContactForm />
-      <Separator />
-      <motion.div
-        className="w-full flex flex-col flex-wrap md:flex-row items-center"
-        variants={containerVariants}
-        initial="hidden"
-        animate={loading ? "hidden" : "visible"}
-      >
-        {loading
-          ? // Skeleton Loading
-            Array(3)
-              .fill(null)
-              .map((_, index) => (
-                <div
-                  key={index}
-                  className="flex flex-col items-center justify-center gap-4"
-                >
-                  {/* Skeleton for Link */}
-                  <Skeleton className="w-60 h-11 rounded-full" />
-                </div>
-              ))
-          : // Contact Link Data Rendering
-            contactlinks.map((contactLink) => (
-              <Link
-                key={contactLink.id}
-                href={contactLink.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="relative flex items-center"
-              >
-                {contactLink.image !== null && (
-                  <Image
-                    src={contactLink.image}
-                    alt={contactLink.name}
-                    width={400}
-                    height={400}
-                    className="min-w-8 max-w-8 transition-all duration-300"
-                  />
-                )}
-              </Link>
-            ))}
-      </motion.div>
+      <div className="w-full flex flex-col items-center gap-4 text-white">
+        <div className="w-full flex items-center justify-center gap-1">
+          <div className="h-[1px] w-full bg-white"></div>
+          <span className="mx-3">OR</span>
+          <div className="h-[1px] w-full bg-white"></div>
+        </div>
+        <div className="w-full flex items-center justify-center gap-2 mt-4">
+          {contactlinks.map((contactLink) => (
+            <Link
+              key={contactLink.id}
+              href={contactLink.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="relative flex items-center"
+            >
+              {contactLink.image !== null && (
+                <Image
+                  src={contactLink.image}
+                  alt={contactLink.name}
+                  width={400}
+                  height={400}
+                  className="min-w-6 max-w-6 transition-all duration-300"
+                />
+              )}
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
