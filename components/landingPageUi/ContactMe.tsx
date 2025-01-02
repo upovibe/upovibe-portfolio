96dd9eccae0d3a55627f216ed1b53fe048f47f8c
upovibe <uzorpromiseokwudiri@gmail.com>
2025-01-02 16:44:55 +0000

    Refactor ContactMe component: remove unused Separator import

--- components/landingPageUi/ContactMe.tsx
+++ components/landingPageUi/ContactMe.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import ContactForm from "@/components/form/ContactForm";
-import { Separator } from "../ui/separator";
 
 interface ContactLink {
   id: string;
